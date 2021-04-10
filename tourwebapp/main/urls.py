891da478678014d30009fa79ca433e37b7ffe339
AshimGhising <np03a190107@heraldcollege.edu.np>
2021-04-10 13:39:22 +0545

    created aboutus page and sliders and also added images

--- tourwebapp/main/urls.py
+++ tourwebapp/main/urls.py
@@ -6,4 +6,5 @@ from . import views
 
 urlpatterns = [
     path("",views.index, name = "index"), # this code serves the http response from the views.py
+    path("about", views.about, name = "about"),
 ]
--- tourwebapp/main/views.py
+++ tourwebapp/main/views.py
@@ -6,3 +6,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "main/home.html", {})
+
+def about(request):
+    return render(request, "main/about.html" )
